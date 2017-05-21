0104b558671ffa03b11f6a9c0e95248382e82bdc
redbattousai <kchangfatt@gmail.com>
2017-05-21 18:22:32 -0400

    Added route to get a single champion

--- routes/static.js
+++ routes/static.js
@@ -3,22 +3,33 @@ var router = express.Router();
 var env = require('../environment');
 var https = require('https');
 
-router.get('/anubis/champion_service/v1/api/champions', function(req, res, next){
-    var request = https.get(env.host + '/api/lol/static-data/NA/v1.2/champion?champData=tags&api_key=' + env.apiKey, function (response) {
-        var data;
-        console.log('statusCode:', response.statusCode);
-        console.log('headers:', response.headers);
-        response.on('data', function(chunk) {
-            data = chunk;
-        });
-        response.on('error', function(error) {
-            console.log('Error: ' + error);
-            return next(error);
-        });
+function processRequest(response, res) {
+    var data;
+    console.log('statusCode:', response.statusCode);
+    console.log('headers:', response.headers);
+    response.on('data', function(chunk) {
+        data = chunk;
+    });
+    response.on('error', function(error) {
+        console.log('Error: ' + error);
+        return next(error);
+    });
+    response.on('end', function() {
+        res.send(data);
+    });
+}
 
-        response.on('end', function() {
-            res.send(data);
-        });
+router.get('/anubis/champion_service/v1/api/champion', function(req, res, next) {
+    var request = https.get(env.host + '/api/lol/static-data/NA/v1.2/champion?champData=tags&api_key=' + env.apiKey, function(response) {
+        processRequest(response, res);
+    });
+    request.end();
+});
+
+router.get('/anubis/champion_service/v1/api/champion/:id', function(req, res, next) {
+    console.log(req.params.id);
+    var request = https.get(env.host + '/api/lol/static-data/NA/v1.2/champion/' + req.params.id + '?champData=all&api_key=' + env.apiKey, function(response) {
+        processRequest(response, res);
     });
     request.end();
 });
